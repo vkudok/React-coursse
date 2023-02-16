858a9a15a79f02f8e19ed6440cfd38f1cde7205b
ArchieSA <s.archie.dev@gmail.com>
2023-02-16 22:14:25 +0300

    lesson2: add jsx

--- src/index.js
+++ src/index.js
@@ -1,20 +1,56 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Button } from "./components/Button/Button";
 import "./index.css";
+
+const SPAN_ID = "123";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  React.createElement("div", {
-    id: "theFirstElement",
-    children: [
-      React.createElement("div", { children: "Hello!" }),
-      React.createElement("div", { children: "" }),
-      React.createElement("div", { children: "Students!" }),
-      React.createElement("button", {
-        children: "ClickMe!",
-        onClick: () => console.log("onClick"),
-      }),
-      React.createElement("img", { src: "", alt: "Hello Img" }),
-    ],
-  })
+  <div>
+    <Button onClick={() => console.log("click")}>Заказать</Button>
+    <Button onClick={() => console.log("cancel")}>Отменить</Button>
+    <span id={SPAN_ID}>Hello</span>
+  </div>
 );
+
+// React.createElement("div", {
+//   children: [
+//     React.createElement(Button, {
+//       children: "Заказать",
+//       onClick: () => console.log("click"),
+//     }),
+//     React.createElement(Button, {
+//       children: "Отменить",
+//       onClick: () => console.log("cancel"),
+//     }),
+//     React.createElement("span", { children: "Hello", id: "123" }),
+//   ],
+// })
+
+// class MyButton {
+//   x;
+//   y;
+//   constructor(x, y) {
+//     this.x = x;
+//     this.y = y;
+//   }
+//   click() {}
+// }
+
+// function MyButton(x, y) {
+//   this.x = x;
+//   this.y = y;
+// }
+
+// MyButton.prototype.click = function () {
+//   console.log(this.x);
+// };
+
+// const button = new MyButton(1, 5);
+
+// button.click();
+
+// const clickWithContext = button.click.bind(button);
+
+// clickWithContext();
---
+++ src/pages/RestaurantPage/RestaurantPage.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const RestaurantPage = () => {
+  return <div>RestaurantPage</div>;
+};
